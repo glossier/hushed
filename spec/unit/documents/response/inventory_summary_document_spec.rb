7a817a4c173595275c015a4d3fa95f24762c20e0
Sherry Son <son.sherrysoomin92@gmail.com>
2016-07-05 14:44:16 -0400

    refactor tests to pass with minitest

--- spec/unit/documents/response/inventory_summary_document_spec.rb
+++ spec/unit/documents/response/inventory_summary_document_spec.rb
@@ -12,12 +12,11 @@ module Hushed
           inventory_summary = InventorySummaryDocument.new(io: document)
           inventory_items = inventory_summary.inventory_items
 
-          expect(inventory_summary.client_id).to eq('HUSHED')
-          expect(inventory_summary.message_date.inspect).to eq('2016-06-22 19:36:21 UTC')
-          expect(inventory_summary.business_unit).to eq('HUSHED')
-          expect(inventory_summary.warehouse).to eq('QUIET')
-
-          expect(inventory_summary.item_stat_hash(inventory_items[0])).to eq({ "GBB100" => "10000" })
+          assert_equal 'HUSHED', inventory_summary.client_id
+          assert_equal '2016-06-22 19:36:21 UTC', inventory_summary.message_date.inspect
+          assert_equal 'HUSHED', inventory_summary.business_unit
+          assert_equal 'QUIET', inventory_summary.warehouse
+          assert_equal ({"GBB100"=>"10000"}), inventory_summary.item_stat_hash(inventory_items[0])
         end
 
       end
